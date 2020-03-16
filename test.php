f22894810109e56a5c4b399003b008f3be4b4c1a
Nour Eldeen <37738435+nourborhan@users.noreply.github.com>
2020-03-16 23:16:25 +0200

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body style="background-color:powderblue;">
-
-<h2>HTML Image</h2>
-<img src="WebAppLogo.png" alt="Flowers in Chania" width="460" height="345">
-
-</body>
-</html>
