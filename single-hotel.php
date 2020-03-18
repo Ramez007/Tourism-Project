075d7aee0a88afae2582f86a5b13f4903aa928f6
Ramez007 <45740184+Ramez007@users.noreply.github.com>
2020-03-18 19:40:12 +0200

    Created Single Hotel Page with adjusting some css

---
+++ single-hotel.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Speedo Tours &mdash; License No. 782</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	<!-- <link href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700italic,900,700,900italic' rel='stylesheet' type='text/css'> -->
+
+	<!-- Stylesheets -->
+	<!-- Dropdown Menu -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<!-- Owl Slider -->
+	<!-- <link rel="stylesheet" href="css/owl.carousel.css"> -->
+	<!-- <link rel="stylesheet" href="css/owl.theme.default.min.css"> -->
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.min.css">
+	<!-- CS Select -->
+	<link rel="stylesheet" href="css/cs-select.css">
+	<link rel="stylesheet" href="css/cs-skin-border.css">
+
+	<!-- Themify Icons -->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Flat Icon -->
+	<link rel="stylesheet" href="css/flaticon.css">
+	<!-- Icomoon -->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	
+	<!-- Style -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<div id="fh5co-wrapper">
+	<div id="fh5co-page">
+	<div id="fh5co-header">
+		<header id="fh5co-header-section">
+			<div class="container">
+				<?php 
+				include "header.php";
+				?>
+			</div>
+		</header>
+		
+	</div>
+	<!-- end:fh5co-header -->
+	<div class="fh5co-parallax" style="background-image: url(images/slider1.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
+					<div class="fh5co-intro fh5co-table-cell">
+						<h1 class="text-center">Winter Palace Hotel</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="fh5co-hotel-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-15">
+                        <h3>Description</h3>
+                            <p>The Sofitel Winter Palace Hotel, also known as the Old Winter Palace Hotel, is a historic British colonial-era 5-star luxury resort hotel located on the banks of the River Nile in Luxor, Egypt, just south of Luxor Temple, with 86 rooms and 6 suites. </p>
+                            <p>The hotel was built by the Upper Egypt Hotels Co, an enterprise founded in 1905 by Cairo hoteliers Charles Baehler and George Nungovich in collaboration with Thomas Cook & Son (Egypt). It was inaugurated on Saturday 19 January 1907, with a picnic at the Valley of the Kings followed by dinner at the hotel and speeches.[1]
+
+                            The architect was Leon Stienon, the Italian construction company G.GAROZZO & Figli Costruzioni in Cemento Armato, Sistema SIACCI brevettato.
+
+                            During World War I the hotel was temporarily closed to paying guests and employed as a hospice for convalescing soldiers. A regular guest at the hotel from 1907 on was George Herbert, 5th Earl of Carnarvon, better known simply as Lord Carnarvon. Carnarvon was the patron of Egyptologist Howard Carter, who in 1922 discovered the intact tomb of Tutankhamun. After the discovery was announced the Winter Palace played host to the international press corps and foreign visitors there to follow the story. Carter used the hotel's noticeboard to deliver occasional news and information on the discovery.
+
+                            In 1975 the complex was expanded with the construction of the New Winter Palace. The addition, classified as a 3-star hotel, was joined by corridors to the original. It was demolished in 2008.
+
+                            In 1996, the Pavillon, a 4-star annex with 116 rooms, was built in the rear garden of the Winter Palace, close to the swimming pool. The Pavillon shares many amenities with the Winter Palace, including the gardens, pools, tennis courts, terraces and restaurants.
+
+                            The hotel is owned by the Egyptian General Company for Tourism & Hotels ("EGOTH") of Egypt and managed by Accor, a French Hotel company, where it is part of the prime division Sofitel.
+
+                            The Hotel is featured on the exclusive Palace Hotels of the World.
+
+                            The Winter Palace has 5 restaurants. The 1886 Restaurant, which serves French cuisine, is named after the date the hotel inaccurately advertises that it was founded. It and the la Corniche Restaurant (international cuisine) are both located in the historic Palace wing. The Bougainvilliers (international cuisine) is in the Pavilion wing, while the Palmetto (Italian cuisine and snacks) and the El Tarboush (Egyptian cuisine) are in the garden close to the swimming pool. 
+                            </p>
+                        <h3>Leisure Facilities</h3>
+
+                            <div class="services">
+                                <span><i class="ti-rss-alt"></i></span>
+                                <div class="desc"> Wifi</div>
+                            </div>
+
+                            <div class="services">
+                                <span><i class="flaticon-swimming icon"></i></span>
+                                <div class="desc"> Swimming Pool</div>
+                            </div>
+
+                            <div class="services">
+                                <span><i class="flaticon-bicycle icon"></i></span>
+                                <div class="desc"> Gym</div>
+                            </div>
+
+                            <div class="services">
+                                <span><i class="flaticon-massage icon"></i></span>
+                                <div class="desc"> Spa</div>
+                            </div>
+
+                            <div class="services">
+                                <span><i class="flaticon-cup icon"></i></span>
+                                <div class="desc"> Bar</div>
+                            </div>
+
+                            <div class="services">
+                                <span><i class="flaticon-restaurant icon"></i></span>
+                                <div class="desc"> Resturant</div>
+                            </div>
+                            <!-- Booking Section -->
+                        <h3>Book Now</h3> 
+                        <form action="">
+
+                                <div class="a-col alternate">
+                                    <div class="input-field">
+                                        <label for="date-start">Check In</label>
+                                        <input type="text" class="form-control" id="date-start" />
+                                    </div>
+                                </div>
+                                
+
+                                <div class="a-col alternate">
+                                <div class="input-field">
+                                    <label for="date-end">Check Out</label>
+                                    <input type="text" class="form-control" id="date-end" />
+                                </div>
+                                
+                                <div class="quantity-box">   
+                                    <label for="quantity">Number of Adults:</label>
+                                    <input type="number" id="quantity" name="quantity" min="1" max="100">
+                                </div>
+
+                                <div class="quantity-box">   
+                                    <label for="quantity">Number of Children:</label>
+                                    <input type="number" id="quantity" name="quantity" min="0" max="100">
+                                </div>
+
+                                <div class="boardtype">
+                                    <input type="radio" name="boardtype" value="fullboard"> Full Board<br>
+                                    <input type="radio" name="boardtype" value="halfboard"> Half Board<br>
+                                </div>
+                                
+                                <div class="quantity-box">   
+                                    <label for="quantity">Choose Number of Single Rooms</label>
+                                    <input type="number" id="quantity" name="quantity" min="0" max="20">
+                                </div>
+                                 
+                                <div class="quantity-box">   
+                                    <label for="quantity">Choose Number of Double Rooms:</label>
+                                    <input type="number" id="quantity" name="quantity" min="0" max="20">
+                                </div>
+
+                                <div class="quantity-box">   
+                                    <label for="quantity">Choose Number of Triple Rooms:</label>
+                                    <input type="number" id="quantity" name="quantity" min="0" max="20">
+                                </div>
+
+                                <div class="quantity-box">   
+                                    <label for="quantity">Choose Number of Suits:</label>
+                                    <input type="number" id="quantity" name="quantity" min="0" max="5">
+                                </div>
+
+                                <input type="submit" value="Book" class="btn btn-primary btn-lg">
+                                
+
+
+                        </form>
+                        <!-- end booking section     -->
+
+						</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	
+
+	<footer id="footer" class="fh5co-bg-color">
+	<?php
+	include "Footer.php"
+	?>
+	</footer>
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+	
+	<!-- Javascripts -->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<!-- Dropdown Menu -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Owl Slider -->
+	<!-- // <script src="js/owl.carousel.min.js"></script> -->
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.min.js"></script>
+	<!-- CS Select -->
+	<script src="js/classie.js"></script>
+	<script src="js/selectFx.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+
+    <script src="js/custom.js"></script>
+    <script src="js/multi-level-drop.js"></script>
+
+</body>
+</html>
