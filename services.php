2af560a773f3077d1acee0c19b6096e042aa0646
Ramez007 <45740184+Ramez007@users.noreply.github.com>
2020-03-18 15:24:10 +0200

    Text and Interface Updates
    1- The Hotels page interface is carried into packages page
    
    2- uploaded a new pic to the pacakges page (below header pic)
    
    3- increased the height of the title picture section in all pages except home. that was done by updating the height from 600 to 650 and that infected all pages execpt the home

--- services.php
+++ services.php
@@ -119,7 +119,7 @@
 		
 	</div>
 	<!-- end:fh5co-header -->
-	<div class="fh5co-parallax" style="background-image: url(images/slider1.jpg);" data-stellar-background-ratio="0.5">
+	<div class="fh5co-parallax" style="background-image: url(images/packagehomepic.jpg);" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
@@ -133,63 +133,82 @@
 	</div>
 
 
-	<div id="fh5co-services-section">
+	<div id="fh5co-hotel-section">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-4">
-					<div class="services">
-						<span><i class="ti-location-pin"></i></span>
+					<div class="hotel-content">
+						<div class="hotel-grid" style="background-image: url(images/image-1.jpg);">
+							<div class="price"><small>For as low as</small><span>$100/night</span></div>
+							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
+						</div>
 						<div class="desc">
-							<h3>Accessible Location</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<h3><a href="#">Package Name</a></h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="services">
-						<span><i class="ti-alarm-clock"></i></span>
+					<div class="hotel-content">
+						<div class="hotel-grid" style="background-image: url(images/image-2.jpg);">
+							<div class="price"><small>For as low as</small><span>$100/night</span></div>
+							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
+						</div>
 						<div class="desc">
-							<h3>Open 24/7</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<h3><a href="#">Package Name</a></h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="services">
-						<span><i class="ti-calendar"></i></span>
+					<div class="hotel-content">
+						<div class="hotel-grid" style="background-image: url(images/image-3.jpg);">
+							<div class="price"><small>For as low as</small><span>$100/night</span></div>
+							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
+						</div>
 						<div class="desc">
-							<h3>Reservation</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<h3><a href="#">Package Name</a></h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="services">
-						<span><i class="ti-user"></i></span>
+					<div class="hotel-content">
+						<div class="hotel-grid" style="background-image: url(images/image-4.jpg);">
+							<div class="price"><small>For as low as</small><span>$100/night</span></div>
+							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
+						</div>
 						<div class="desc">
-							<h3>Friendly Staff</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<h3><a href="#">Package Name</a></h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="services">
-						<span><i class="ti-signal"></i></span>
+					<div class="hotel-content">
+						<div class="hotel-grid" style="background-image: url(images/image-5.jpg);">
+							<div class="price"><small>For as low as</small><span>$100/night</span></div>
+							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
+						</div>
 						<div class="desc">
-							<h3>Free Wifi</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<h3><a href="#">Package Name</a></h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 						</div>
 					</div>
 				</div>
 				<div class="col-md-4">
-					<div class="services">
-						<span><i class="ti-location-pin"></i></span>
+					<div class="hotel-content">
+						<div class="hotel-grid" style="background-image: url(images/image-6.jpg);">
+							<div class="price"><small>For as low as</small><span>$100/night</span></div>
+							<a class="book-now text-center" href="#"><i class="ti-calendar"></i> Book Now</a>
+						</div>
 						<div class="desc">
-							<h3>Accessible Location</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<h3><a href="#">Package Name</a></h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 						</div>
 					</div>
 				</div>
+
 			</div>
 		</div>
 	</div>
