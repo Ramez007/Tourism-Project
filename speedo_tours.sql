aa469c676c8b2de68a030a99453b809351672aa3
Ramez007 <45740184+Ramez007@users.noreply.github.com>
2020-03-30 21:14:40 +0200

    Updated DB

--- speedo_tours.sql
+++ speedo_tours.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2020 at 08:13 PM
+-- Generation Time: Mar 30, 2020 at 09:04 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -46,6 +46,257 @@ CREATE TABLE `cruise` (
 INSERT INTO `cruise` (`CruiseID`, `CruiseName`, `NumberofCabins`, `Captain`, `Pets`, `Fishing`, `SunBathing`, `Pool`) VALUES
 (1, 'RMS Titanic', 2, 'Edward Smith', 'TRUE', 'TRUE', '', 'TRUE');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `EmployeeID` int(11) NOT NULL,
+  `Name` varchar(60) NOT NULL,
+  `JobType` set('ADMIN','SUPPORT','TOURGUIDE') NOT NULL,
+  `Email` varchar(60) NOT NULL,
+  `Username` varchar(60) NOT NULL,
+  `Password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`EmployeeID`, `Name`, `JobType`, `Email`, `Username`, `Password`) VALUES
+(3, 'John Doe', 'ADMIN', 'test@test', 'test', 'test'),
+(4, 'Al Pacino', 'SUPPORT', 'test1@test1', 'test1', 'test1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `PictureId` int(11) NOT NULL,
+  `PackageId` int(11) DEFAULT NULL,
+  `HotelId` int(11) DEFAULT NULL,
+  `picture` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guest`
+--
+
+CREATE TABLE `guest` (
+  `GuestID` int(11) NOT NULL,
+  `FirstName` varchar(20) NOT NULL,
+  `LastName` varchar(20) NOT NULL,
+  `Gender` set('MALE','FEMALE') NOT NULL,
+  `Age` int(3) NOT NULL,
+  `NationalID` int(11) NOT NULL,
+  `PassportNumber` int(9) NOT NULL,
+  `City` varchar(15) NOT NULL,
+  `Country` varchar(15) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `BankAccount` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `guest`
+--
+
+INSERT INTO `guest` (`GuestID`, `FirstName`, `LastName`, `Gender`, `Age`, `NationalID`, `PassportNumber`, `City`, `Country`, `Email`, `Username`, `Password`, `BankAccount`) VALUES
+(1, 'Robert', 'Deniro', 'MALE', 60, 2020, 9999, 'Corleone', 'Italy', 'test', 'test', 'test', 2112),
+(2, 'Sean', 'Connery', 'MALE', 75, 0, 0, 'London', 'England', 'test@dsafsda', 'ramez', 'test', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotel`
+--
+
+CREATE TABLE `hotel` (
+  `HotelID` int(11) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `NumberofRooms` int(11) NOT NULL,
+  `WiFI` set('TRUE','FALSE') NOT NULL,
+  `Swimming Pool` set('TRUE','FALSE') NOT NULL,
+  `RESORT` set('TRUE','FALSE') NOT NULL,
+  `GYM` set('TRUE','FALSE') NOT NULL,
+  `Full_Board` set('TRUE','FALSE') NOT NULL,
+  `Half_Board` set('TRUE','FALSE') NOT NULL,
+  `Pets` set('TRUE','FALSE') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hotel`
+--
+
+INSERT INTO `hotel` (`HotelID`, `Name`, `NumberofRooms`, `WiFI`, `Swimming Pool`, `RESORT`, `GYM`, `Full_Board`, `Half_Board`, `Pets`) VALUES
+(1, 'Ritz Carlton', 500, 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE', 'TRUE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inquiries`
+--
+
+CREATE TABLE `inquiries` (
+  `InquiryID` int(11) NOT NULL,
+  `EmployeeID` int(11) DEFAULT NULL,
+  `Name` varchar(12) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `Inquiry` varchar(600) NOT NULL,
+  `TimeStamp` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `inquiries`
+--
+
+INSERT INTO `inquiries` (`InquiryID`, `EmployeeID`, `Name`, `Email`, `Inquiry`, `TimeStamp`) VALUES
+(1, 3, 'Robert', 'test@valid', 'some text', '2020-03-16 19:52:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `languages`
+--
+
+CREATE TABLE `languages` (
+  `LanguageID` int(11) NOT NULL,
+  `Employee ID` int(11) NOT NULL,
+  `LanguageName` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `languages`
+--
+
+INSERT INTO `languages` (`LanguageID`, `Employee ID`, `LanguageName`) VALUES
+(1, 3, 'English');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newswire`
+--
+
+CREATE TABLE `newswire` (
+  `ID` int(11) NOT NULL,
+  `Email` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+
+CREATE TABLE `packages` (
+  `PackageID` int(11) NOT NULL,
+  `PackageName` varchar(60) NOT NULL,
+  `ReserveLimit` int(11) NOT NULL,
+  `CruiseID` int(11) DEFAULT NULL,
+  `HotelID` int(11) DEFAULT NULL,
+  `Price` int(11) NOT NULL,
+  `TourGuideID` int(11) DEFAULT NULL,
+  `Transportation` set('TRUE','FALSE') NOT NULL,
+  `NumberofDays` int(11) NOT NULL,
+  `NumberofNights` int(11) NOT NULL,
+  `Suspended` set('TRUE','FALSE') NOT NULL,
+  `DateIn` int(11) NOT NULL,
+  `DateOut` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`PackageID`, `PackageName`, `ReserveLimit`, `CruiseID`, `HotelID`, `Price`, `TourGuideID`, `Transportation`, `NumberofDays`, `NumberofNights`, `Suspended`, `DateIn`, `DateOut`) VALUES
+(3, 'Rome/Milano', 40, 1, 1, 500, 3, 'TRUE', 50, 51, '', 21, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reserves`
+--
+
+CREATE TABLE `reserves` (
+  `ReserveID` int(11) NOT NULL,
+  `GuestId` int(11) DEFAULT NULL,
+  `PackageId` int(11) DEFAULT NULL,
+  `HotelId` int(11) DEFAULT NULL,
+  `NoofChildren` int(11) NOT NULL,
+  `NoofAdults` int(11) NOT NULL,
+  `Date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reserves`
+--
+
+INSERT INTO `reserves` (`ReserveID`, `GuestId`, `PackageId`, `HotelId`, `NoofChildren`, `NoofAdults`, `Date`) VALUES
+(1, 1, NULL, 1, 65, 2, '2020-03-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `ReviewID` int(11) NOT NULL,
+  `GuestID` int(11) DEFAULT NULL,
+  `PackageID` int(11) DEFAULT NULL,
+  `HotelID` int(11) DEFAULT NULL,
+  `Review` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
+  `RoomID` int(11) NOT NULL,
+  `RoomNumber` int(11) NOT NULL,
+  `RoomType` set('Single','Double','Triple','Royal','Bridal') NOT NULL,
+  `Status` set('Free','Not') NOT NULL,
+  `HotelID` int(11) DEFAULT NULL,
+  `GuestID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stops`
+--
+
+CREATE TABLE `stops` (
+  `StopsID` int(11) NOT NULL,
+  `StopNumber` int(11) NOT NULL,
+  `StopName` varchar(60) NOT NULL,
+  `CruiseID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visits`
+--
+
+CREATE TABLE `visits` (
+  `VisitID` int(11) NOT NULL,
+  `NameLocation` varchar(60) NOT NULL,
+  `VisitDay` int(11) NOT NULL,
+  `PackageID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -57,6 +308,101 @@ ALTER TABLE `cruise`
   ADD PRIMARY KEY (`CruiseID`);
 
 --
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`EmployeeID`);
+
+--
+-- Indexes for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`PictureId`),
+  ADD KEY `PackageId` (`PackageId`),
+  ADD KEY `HotelId` (`HotelId`);
+
+--
+-- Indexes for table `guest`
+--
+ALTER TABLE `guest`
+  ADD PRIMARY KEY (`GuestID`);
+
+--
+-- Indexes for table `hotel`
+--
+ALTER TABLE `hotel`
+  ADD PRIMARY KEY (`HotelID`);
+
+--
+-- Indexes for table `inquiries`
+--
+ALTER TABLE `inquiries`
+  ADD PRIMARY KEY (`InquiryID`),
+  ADD KEY `EmployeeID` (`EmployeeID`);
+
+--
+-- Indexes for table `languages`
+--
+ALTER TABLE `languages`
+  ADD PRIMARY KEY (`LanguageID`),
+  ADD KEY `Employee ID` (`Employee ID`);
+
+--
+-- Indexes for table `newswire`
+--
+ALTER TABLE `newswire`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `packages`
+--
+ALTER TABLE `packages`
+  ADD PRIMARY KEY (`PackageID`),
+  ADD KEY `HotelID` (`HotelID`),
+  ADD KEY `CruiseID` (`CruiseID`),
+  ADD KEY `TourGuideID` (`TourGuideID`);
+
+--
+-- Indexes for table `reserves`
+--
+ALTER TABLE `reserves`
+  ADD PRIMARY KEY (`ReserveID`),
+  ADD KEY `GuestId` (`GuestId`),
+  ADD KEY `HotelId` (`HotelId`),
+  ADD KEY `PackageId` (`PackageId`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`ReviewID`),
+  ADD KEY `GuestID` (`GuestID`),
+  ADD KEY `HotelID` (`HotelID`),
+  ADD KEY `CruiseID` (`PackageID`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`RoomID`),
+  ADD KEY `GuestID` (`GuestID`),
+  ADD KEY `HotelID` (`HotelID`);
+
+--
+-- Indexes for table `stops`
+--
+ALTER TABLE `stops`
+  ADD PRIMARY KEY (`StopsID`),
+  ADD KEY `CruiseID` (`CruiseID`);
+
+--
+-- Indexes for table `visits`
+--
+ALTER TABLE `visits`
+  ADD PRIMARY KEY (`VisitID`),
+  ADD KEY `PackageID` (`PackageID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,6 +411,150 @@ ALTER TABLE `cruise`
 --
 ALTER TABLE `cruise`
   MODIFY `CruiseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `EmployeeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `PictureId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `guest`
+--
+ALTER TABLE `guest`
+  MODIFY `GuestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `hotel`
+--
+ALTER TABLE `hotel`
+  MODIFY `HotelID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `inquiries`
+--
+ALTER TABLE `inquiries`
+  MODIFY `InquiryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `languages`
+--
+ALTER TABLE `languages`
+  MODIFY `LanguageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `newswire`
+--
+ALTER TABLE `newswire`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `packages`
+--
+ALTER TABLE `packages`
+  MODIFY `PackageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `reserves`
+--
+ALTER TABLE `reserves`
+  MODIFY `ReserveID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `ReviewID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rooms`
+--
+ALTER TABLE `rooms`
+  MODIFY `RoomID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stops`
+--
+ALTER TABLE `stops`
+  MODIFY `StopsID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `visits`
+--
+ALTER TABLE `visits`
+  MODIFY `VisitID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `gallery`
+--
+ALTER TABLE `gallery`
+  ADD CONSTRAINT `gallery_ibfk_1` FOREIGN KEY (`PackageId`) REFERENCES `packages` (`PackageID`),
+  ADD CONSTRAINT `gallery_ibfk_2` FOREIGN KEY (`HotelId`) REFERENCES `hotel` (`HotelID`);
+
+--
+-- Constraints for table `inquiries`
+--
+ALTER TABLE `inquiries`
+  ADD CONSTRAINT `inquiries_ibfk_1` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`EmployeeID`);
+
+--
+-- Constraints for table `languages`
+--
+ALTER TABLE `languages`
+  ADD CONSTRAINT `languages_ibfk_1` FOREIGN KEY (`Employee ID`) REFERENCES `employees` (`EmployeeID`);
+
+--
+-- Constraints for table `packages`
+--
+ALTER TABLE `packages`
+  ADD CONSTRAINT `packages_ibfk_1` FOREIGN KEY (`CruiseID`) REFERENCES `cruise` (`CruiseID`),
+  ADD CONSTRAINT `packages_ibfk_2` FOREIGN KEY (`HotelID`) REFERENCES `hotel` (`HotelID`),
+  ADD CONSTRAINT `packages_ibfk_3` FOREIGN KEY (`TourGuideID`) REFERENCES `languages` (`Employee ID`);
+
+--
+-- Constraints for table `reserves`
+--
+ALTER TABLE `reserves`
+  ADD CONSTRAINT `reserves_ibfk_1` FOREIGN KEY (`GuestId`) REFERENCES `guest` (`GuestID`),
+  ADD CONSTRAINT `reserves_ibfk_2` FOREIGN KEY (`HotelId`) REFERENCES `hotel` (`HotelID`),
+  ADD CONSTRAINT `reserves_ibfk_3` FOREIGN KEY (`PackageId`) REFERENCES `packages` (`PackageID`);
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`GuestID`) REFERENCES `guest` (`GuestID`),
+  ADD CONSTRAINT `reviews_ibfk_3` FOREIGN KEY (`PackageID`) REFERENCES `packages` (`PackageID`),
+  ADD CONSTRAINT `reviews_ibfk_4` FOREIGN KEY (`HotelID`) REFERENCES `hotel` (`HotelID`);
+
+--
+-- Constraints for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD CONSTRAINT `rooms_ibfk_2` FOREIGN KEY (`HotelID`) REFERENCES `hotel` (`HotelID`),
+  ADD CONSTRAINT `rooms_ibfk_3` FOREIGN KEY (`GuestID`) REFERENCES `guest` (`GuestID`);
+
+--
+-- Constraints for table `stops`
+--
+ALTER TABLE `stops`
+  ADD CONSTRAINT `stops_ibfk_1` FOREIGN KEY (`CruiseID`) REFERENCES `cruise` (`CruiseID`);
+
+--
+-- Constraints for table `visits`
+--
+ALTER TABLE `visits`
+  ADD CONSTRAINT `visits_ibfk_1` FOREIGN KEY (`PackageID`) REFERENCES `packages` (`PackageID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
