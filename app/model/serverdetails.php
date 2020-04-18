2e16eb0286e9cba680f704ec507de4ee36aa53b4
ahmedmahdy1899 <ahmed.mahdy1899@gmail.com>
2020-04-18 19:29:58 +0200

    el inquiry keda kmn sha3'ala

--- app/model/serverdetails.php
+++ app/model/serverdetails.php
@@ -16,14 +16,15 @@ try {
     //Server settings
   //  $email->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $email->isSMTP();                                            // Send using SMTP
-    $email->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+   $email->isHTML(true);
+   $email->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $email->SMTPAuth   = true;                                   // Enable SMTP authentication
     $email->SingleTo = true;
     $email->Username   = 'ahmed1707083@miuegypt.edu.eg';                     // SMTP username
     $email->Password   = 'ahmedmahdy99';                               // SMTP password
     $email->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $email->Port       = 587  ;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-    $email->SetFrom('ahmed1707083@miuegypt.edu.eg');
+  
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
---
+++ app/model/write_inquiries_model.php
@@ -0,0 +1,48 @@
+<html>
+<head><script src="js/sweetalert.min.js"></script></head>
+
+<body>
+<?php
+
+require_once("app/model/model.php");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'C:\xampp\composer\vendor\autoload.php';
+?>
+
+<?php
+
+class writeinquiry extends Model {
+
+    private $dbh;
+
+    function __construct() {
+        $this->dbh = $this->connect();
+    }
+
+    function write_inquiries()
+    { include_once "serverdetails.php";
+        $name=$_POST['name'];
+        $mail=$_POST['sendermail'];
+        $message=$_POST['message'];
+        try{
+            $sql = "INSERT INTO inquiries (author,Email,Inquiry) values ('$name','$mail','$message')" ; 
+             $result = mysqli_query($this->dbh->getConn(),$sql) ;
+             $email->addAddress($mail);
+             $email->Subject="inquiries";
+             $email->Body=$message;
+             $email->SetFrom("$mail",$name);
+             $email->send();
+             echo'<script>swal("Successfully Sent", "", "success");</script>'; 
+    }catch(Exception $e){
+        echo $e->errorMessage();
+     
+    } 
+            
+    }
+
+}
+
+?>
+</body>
+</html>
